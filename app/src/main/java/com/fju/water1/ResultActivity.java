6df59cdd28e5a8e9f99675c1ee833e9a3516e452
miia <miia@com.tw>
2019-11-19 15:17:32 +0800

    11/19/1

--- app/src/main/java/com/fju/water1/ResultActivity.java
+++ app/src/main/java/com/fju/water1/ResultActivity.java
@@ -2,7 +2,10 @@ package com.fju.water1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        float money = getIntent().getFloatExtra("FEE",-1);
+        Log.d("ResultActivity",money+"");
+        TextView feeView = findViewById(R.id.fee);
+        int n = (int)(money + 0.5f);  //四舍五入整数
+        float m = (int)((money+0.05f)*10);
+        feeView.setText(m/10 + "");
     }
 }
