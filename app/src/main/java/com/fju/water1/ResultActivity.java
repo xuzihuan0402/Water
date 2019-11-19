28c23199f2ecf14964301219752fb6b279604233
miia <miia@com.tw>
2019-11-19 16:29:05 +0800

    11/19/2

--- app/src/main/java/com/fju/water1/ResultActivity.java
+++ app/src/main/java/com/fju/water1/ResultActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class ResultActivity extends AppCompatActivity {
 
     @Override
@@ -18,6 +21,8 @@ public class ResultActivity extends AppCompatActivity {
         TextView feeView = findViewById(R.id.fee);
         int n = (int)(money + 0.5f);  //四舍五入整数
         float m = (int)((money+0.05f)*10);
-        feeView.setText(m/10 + "");
+        DecimalFormat df = new DecimalFormat("###0.0");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        feeView.setText(df.format(money) + "");
     }
 }
