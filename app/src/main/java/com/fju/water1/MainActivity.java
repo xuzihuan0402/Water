0b0f60cbb10eb8516d1a6f455f37b5fbcea3bc58
miia <miia@com.tw>
2019-11-26 16:28:03 +0800

    11/26/3

--- app/src/main/java/com/fju/water1/MainActivity.java
+++ app/src/main/java/com/fju/water1/MainActivity.java
@@ -16,14 +16,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edNext;
     private EditText edMonth;
     private float month;
-    private float next;
+    Boolean isNext = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         edMonth = findViewById(R.id.month);
-        edNext = findViewById(R.id.next);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 money();
+            }
+        });
+        Switch sw = findViewById(R.id.sw);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isNext = isChecked;
+                TextView text = findViewById(R.id.type);
+                text.setText(isNext ? getString(R.string.every_other_month ) : getString(R.string.monthly));
             }
         });
 
@@ -78,17 +88,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void money(){
         float money = 0;
-        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                edMonth.setText("");
-                edNext.setText("");
-
-            }
-        };
+//        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                edMonth.setText("");
+//
+//            }
+//        };
 
         if(edMonth.getText().toString().length()!=0){
             month = Float.parseFloat(edMonth.getText().toString());
+            if(isNext = false){
+
+
             if(month >=1&& month <=10){
                 money = month * 7.35f;
             }
@@ -101,6 +113,20 @@ public class MainActivity extends AppCompatActivity {
             if (month >= 51) {
                 money = month * 12.075f - 110.25f;
             }
+            }else{
+                if(month >=1&& month <=20){
+                money = month * 7.35f;
+            }
+            if (month >= 21 && month <= 60) {
+                money = month * 9.45f - 42;
+            }
+            if (month >= 61 && month <= 100) {
+                money = month * 11.55f - 168;
+            }
+            if (month >= 101) {
+                money = month * 12.075f - 220.5f;
+            }
+            }
             Intent intent = new Intent(this,ResultActivity.class);
             intent.putExtra(getString(R.string.extra_fee), money);
             startActivity(intent);
@@ -111,32 +137,32 @@ public class MainActivity extends AppCompatActivity {
 //                    .setPositiveButton("OK",listener)
 //                    .show();
         }
-        if(edNext.getText().toString().length()!=0){
-            next = Float.parseFloat(edNext.getText().toString());
-            if(next >=1&& next <=20){
-                money = 0;
-            }
-            if (next >= 21 && next <= 60) {
-                money = next * 9.45f - 42;
-            }
-            if (next >= 61 && next <= 100) {
-                money = next * 11.55f - 168;
-            }
-            if (next >= 101) {
-                money = next * 12.075f - 220.5f;
-            }
-//            new AlertDialog.Builder(MainActivity.this)
-//                    .setTitle("隔月抄表费用")
-//                    .setMessage(getString(R.string.fee)+money)
-//                    .setPositiveButton(getString(R.string.ok),listener )
+//        if(edNext.getText().toString().length()!=0){
+//            next = Float.parseFloat(edNext.getText().toString());
+//            if(next >=1&& next <=20){
+//                money = 0;
+//            }
+//            if (next >= 21 && next <= 60) {
+//                money = next * 9.45f - 42;
+//            }
+//            if (next >= 61 && next <= 100) {
+//                money = next * 11.55f - 168;
+//            }
+//            if (next >= 101) {
+//                money = next * 12.075f - 220.5f;
+//            }
+////            new AlertDialog.Builder(MainActivity.this)
+////                    .setTitle("隔月抄表费用")
+////                    .setMessage(getString(R.string.fee)+money)
+////                    .setPositiveButton(getString(R.string.ok),listener )
+////                    .show();
+//        }
+//        if(edMonth.getText().toString().length()==0&&edNext.getText().toString().length()==0){
+//            new AlertDialog.Builder(this)
+//                    .setTitle("错误")
+//                    .setMessage("请输入数字")
+//                    .setPositiveButton("OK",null)
 //                    .show();
-        }
-        if(edMonth.getText().toString().length()==0&&edNext.getText().toString().length()==0){
-            new AlertDialog.Builder(this)
-                    .setTitle("错误")
-                    .setMessage("请输入数字")
-                    .setPositiveButton("OK",null)
-                    .show();
-        }
+//        }
     }
 }
