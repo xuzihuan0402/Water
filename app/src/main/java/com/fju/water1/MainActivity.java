561ed7f0fe00a6b70e6137197eb2a1868256240d
miia <miia@com.tw>
2019-12-03 15:56:31 +0800

    1203/1

--- app/src/main/java/com/fju/water1/MainActivity.java
+++ app/src/main/java/com/fju/water1/MainActivity.java
@@ -12,17 +12,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private EditText edMonth;
     private float month;
     Boolean isNext = false;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Log.d(TAG,"onCreate");
         setSupportActionBar(toolbar);
         edMonth = findViewById(R.id.month);
 
@@ -60,6 +65,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Spinner
+        Spinner cities = findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG,getResources().getStringArray(R.array.cities)[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 
     @Override
@@ -100,7 +155,6 @@ public class MainActivity extends AppCompatActivity {
             month = Float.parseFloat(edMonth.getText().toString());
             if(isNext = false){
 
-
             if(month >=1&& month <=10){
                 money = month * 7.35f;
             }
@@ -113,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
             if (month >= 51) {
                 money = month * 12.075f - 110.25f;
             }
-            }else{
+            }else if(isNext = true){
                 if(month >=1&& month <=20){
                 money = month * 7.35f;
             }
